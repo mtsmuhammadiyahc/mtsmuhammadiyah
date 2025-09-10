ac4e329641d16a91a171f463f208eae317628202
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-11 01:22:35 +0700

    Update adminApi.js

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -11,7 +11,7 @@ const instance = axios.create({
 
 // attach token automatically if exists
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('admin_token');
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
