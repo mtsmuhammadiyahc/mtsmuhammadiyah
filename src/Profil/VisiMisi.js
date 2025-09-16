7ed2820853743d5d64078004e447ef226c408b9a
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-16 13:39:51 +0700

    Update VisiMisi.js

--- src/Profil/VisiMisi.js
+++ src/Profil/VisiMisi.js
@@ -4,7 +4,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./VisiMisi.css";
 
-onst VisiMisi = () => {
+const VisiMisi = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
