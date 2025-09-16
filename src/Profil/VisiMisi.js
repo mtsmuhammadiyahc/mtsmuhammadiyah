4a62168586e0db311c2169ee362b028e60da6959
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-16 12:17:00 +0700

    Update VisiMisi.js

--- src/Profil/VisiMisi.js
+++ src/Profil/VisiMisi.js
@@ -18,7 +18,7 @@ const VisiMisi = () => {
 
   useEffect(() => {
     axios
-      .get(`https://be-production-d9fe.up.railway.app/api/profil/visi-misi`)
+      .get(`https://be-production-d9fe.up.railway.app/api/visi-misi`)
       .then((res) => {
       const result = parseProfilResponse(res, "visi-misi");
       setData(result);
