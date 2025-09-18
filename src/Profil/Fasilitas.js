52260c546c6641cdfc8266aa7bbf6d42c98f7783
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-18 15:56:00 +0700

    Update Fasilitas.js

--- src/Profil/Fasilitas.js
+++ src/Profil/Fasilitas.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "./Profil.css";
+
 
 const Fasilitas = () => {
   const [data, setData] = useState([]);
