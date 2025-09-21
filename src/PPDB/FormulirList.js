8bd2691b483e8f46b9d8967d48d67df5f6e2368e
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-21 17:25:15 +0700

    Update FormulirList.js

--- src/PPDB/FormulirList.js
+++ src/PPDB/FormulirList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./FormulirPPDB.css";
+import "./FormulirList.css";
 
 const FormulirList = () => {
   const [pendaftar, setPendaftar] = useState([]);
