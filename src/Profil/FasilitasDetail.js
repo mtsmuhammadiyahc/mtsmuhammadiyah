a7a43a458ef2768ab3d76b9884a2eb61be9a813e
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-24 18:56:49 +0700

    Update FasilitasDetail.js

--- src/Profil/FasilitasDetail.js
+++ src/Profil/FasilitasDetail.js
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
+import "./FasilitasDetail.css";
+
 
 const FasilitasDetail = () => {
   const { id } = useParams();
