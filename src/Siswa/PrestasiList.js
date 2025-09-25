517c6d875b05c95bf53d80c93095ceb3200afefb
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-25 14:17:24 +0700

    Update PrestasiList.js

--- src/Siswa/PrestasiList.js
+++ src/Siswa/PrestasiList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
-import "./PrestasiList.css";
+//import "./PrestasiList.css";
 
 const PrestasiList = () => {
   const { id } = useParams();
