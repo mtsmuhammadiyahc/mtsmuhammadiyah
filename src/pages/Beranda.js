39ee438700b961f3cfb7c4fe0506205a1218751e
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-27 17:13:59 +0700

    Update Beranda.js

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -2,14 +2,12 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import Fasilitas from "../Profil/Fasilitas";
-import Galeri from "../Galeri/Galeri";
-
 const Beranda = () => {
   return (
     <div>
       <HeroSection />
       <Fasilitas />
-      <Galeri />
+      
     </div>
   );
 };
