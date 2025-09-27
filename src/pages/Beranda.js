c69ddfebd5d3bb875181f862a16d69c2df57ac79
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-27 16:52:23 +0700

    Create Beranda.js

---
+++ src/pages/Beranda.js
@@ -0,0 +1,19 @@
+// src/pages/Beranda.js
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import VisiMisi from "../components/VisiMisi";
+import Fasilitas from "../components/Fasilitas";
+import Galeri from "../components/Galeri";
+
+const Beranda = () => {
+  return (
+    <div>
+      <HeroSection />
+      <VisiMisi />
+      <Fasilitas />
+      <Galeri />
+    </div>
+  );
+};
+
+export default Beranda;
