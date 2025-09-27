51e420512785cb89f90706d1873c8d06475c10d1
Matsamu <mtsmuhammadiyahc@gmail.com>
2025-09-27 17:02:38 +0700

    Create HeroSection.js

---
+++ src/components/HeroSection.js
@@ -0,0 +1,20 @@
+// src/components/HeroSection.js
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#0d47a1",
+        color: "white",
+        padding: "4rem 2rem",
+        textAlign: "center",
+      }}
+    >
+      <h1>Selamat Datang di MTs Muhammadiyah Cilacap</h1>
+      <p>"Membentuk Generasi Islami, Cerdas, dan Mandiri"</p>
+    </div>
+  );
+};
+
+export default HeroSection;
